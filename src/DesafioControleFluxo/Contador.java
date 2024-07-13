86a03761bc37a0a42e4d737b73c69cd252839304
Seu Nome <joanap.santos@outlook.com>
2024-07-12 22:55:35 -0300

    first commit

---
+++ src/DesafioControleFluxo/Contador.java
@@ -0,0 +1,33 @@
+package DesafioControleFluxo;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        try{
+            contar(parametroUm, parametroDois);
+        } catch(ParametrosInvalidosException e){
+            System.err.println(e.getMessage());
+            //System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException(); //lança a exceção
+        }
+        int contagem = parametroDois - parametroUm;
+
+        for(int i=1; i<= contagem; i++){
+            System.out.println("imprimindo n° " + i);
+        }
+        
+    }
+}
