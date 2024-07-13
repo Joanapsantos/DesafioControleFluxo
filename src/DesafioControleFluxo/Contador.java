06a7a7a22edb93881c8a2b7401fe64ce6e841ec8
Seu Nome <joanap.santos@outlook.com>
2024-07-12 23:04:41 -0300

    Atualização comentario

--- src/DesafioControleFluxo/Contador.java
+++ src/DesafioControleFluxo/Contador.java
@@ -13,8 +13,7 @@ public class Contador {
         try{
             contar(parametroUm, parametroDois);
         } catch(ParametrosInvalidosException e){
-            System.err.println(e.getMessage());
-            //System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
     }
 
